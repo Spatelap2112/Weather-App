34596633407b46373de7a33c00d65c08fcf7fc37
Spatelap2112 <157763466+Spatelap2112@users.noreply.github.com>
2024-03-06 20:54:06 -0500

    code modification from nested if to switch statements (#4)
    Co-authored-by: Surbhi Patel <surbhip2112@gmail.com>

--- script.js
+++ script.js
@@ -13,18 +13,29 @@ async function checkWeather(city) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
-    } else if (data.weather[0].main == "Snow") {
-      weatherIcon.src = "images/snow.png";
+    // replaced with switch case
+    switch (data.weather[0].main) {
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        break;
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        break;
+      case "Snow":
+        weatherIcon.src = "images/snow.png";
+        break;
+      default:
+        // Default action if none of the cases match
+        break;
     }
 
     doc.querySelector(".weather").style.display = "block";
